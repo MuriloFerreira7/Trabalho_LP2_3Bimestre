b07c3d390f0d1d137517184448ef293bb2a87fc6
MuriloFerreira7 <murilonaoseidoque@gmail.com>
2025-11-10 09:13:31 -0300

    database model created

--- modelo.sql
+++ modelo.sql
@@ -1,15 +1,34 @@
-create database academia;
-use academia;
-create table advertencias(
+create database academias;
+use academias;
+
+create table pessoa(
+	id int primary key auto_increment,
+    nome varchar(150),
+    cpf varchar(15) not null,
+    rg varchar(45),
+    email varchar(150),
+    telefone varchar(50),
+    dataDeNascimento varchar(150),
+    necessidadeEspecial varchar(150)
+);
+
+create table advertencia(
 	id int primary key auto_increment,
     motivo varchar(500),
     penalidade varchar(500),
     data varchar(500),
     id_pessoa int,
-    foreign key(id_pessoa) references pessoas
+    foreign key(id_pessoa) references pessoa(id)
 );
 
-create table avaliacoesFisicas(
+create table aluno(
+    id int primary key auto_increment,
+    id_pessoa int,
+    objetivo varchar(20),
+    foreign key(id_pessoa) references pessoa(id)
+);
+
+create table avaliacaoFisica(
 	id int primary key auto_increment,
     id_aluno int,
     bracoEsquerdo varchar(20),
@@ -24,30 +43,80 @@ create table avaliacoesFisicas(
     bancoDeWells varchar(20),
     altura varchar(20),
     peso varchar(20),
-    foreign key (id_aluno) references aluno
+    foreign key (id_aluno) references aluno(id)
 );
 
-create table pessoas(
+create table funcionario(
 	id int primary key auto_increment,
-    nome varchar(150),
-    cpf varchar(15),
-    rg varchar(45),
-    email varchar(150),
-    telefone varchar(50),
-    dataDeNascimento varchar(150),
-    necessidadeEspecial varchar(150),
-);
-
-create table alunos(
-    id int primary key auto_increment,
     id_pessoa int,
-    objetivo varchar(20),
-    foreign key(id_pessoa) references pessoas
+    areaAtuacao varchar(150),
+    salario varchar(100),
+    foreign key(id_pessoa) references pessoa(id)
 );
 
-create table funcionarios(
-	id int 
-)
+create table exercicio(
+	id int primary key auto_increment,
+    nome varchar(100),
+    conjuntoMuscularAlvo varchar(100),
+    nivelDeDificuldade varchar(100)
+);
 
+create table musculo (
+	id int primary key auto_increment,
+    nome varchar(150) not null
+);
 
+create table exercicio_musculo(
+	exercicio_id int,
+    musculo_id int,
+    tipo enum('principal','secundario') not null,
+    primary key(exercicio_id, musculo_id, tipo),
+    foreign key (exercicio_id) references exercicio(id),
+    foreign key (musculo_id) references musculo(id)
+);
 
+create table area(
+	id int primary key auto_increment,
+    nome varchar(100)
+);
+
+create table area_exercicio(
+	id_exercicio int not null,
+    id_area int not null,
+    primary key(id_exercicio, id_area),
+    foreign key (id_exercicio) references exercicio(id),
+    foreign key (id_area) references area(id)
+);
+
+create table academia(
+	id int primary key auto_increment,
+    nome varchar(100),
+    endereco varchar(200),
+    horarioDeFuncionamento varchar(200)
+);
+
+create table academia_aluno(
+	id_aluno int not null,
+    id_academia int not null,
+    primary key(id_aluno, id_academia),
+    foreign key(id_aluno) references aluno(id),
+    foreign key(id_academia) references academia(id)
+);
+
+create table academia_funcionario(
+	id_funcionario int not null,
+    id_academia int not null,
+    primary key(id_funcionario, id_academia),
+    foreign key(id_funcionario) references funcionario(id),
+    foreign key(id_academia) references academia(id)
+);
+
+create table academia_area(
+	id_area int not null,
+    id_academia int not null,
+    primary key(id_area, id_academia),
+    foreign key(id_area) references area(id),
+    foreign key(id_academia) references academia(id)
+);
+
+																												
